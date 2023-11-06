84c333b70afcd71b3e54f22a4baf6d06217e2002
MD ZAHED IQBAL <zahediiuc221@gmail.com>
2023-11-06 10:39:32 +0600

    first commit

---
+++ src/app/routes/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+
+const moduleRoutes: any[] = [
+  // ... routes
+];
+
+moduleRoutes.forEach(route => router.use(route.path, route.route));
+export default router;
